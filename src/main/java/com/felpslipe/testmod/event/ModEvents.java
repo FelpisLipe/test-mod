35d487c5b24698af0616a47e9d795bab7a3c29d8
FelpisLipe <176348835+FelpisLipe@users.noreply.github.com>
2025-03-07 03:09:41 -0300

    Fixed hammer taking one more durability than supposed to

--- src/main/java/com/felpslipe/testmod/event/ModEvents.java
+++ src/main/java/com/felpslipe/testmod/event/ModEvents.java
@@ -10,6 +10,8 @@ import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.level.BlockEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -17,6 +19,7 @@ import java.util.Set;
 @EventBusSubscriber(modid = TestMod.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
 public class ModEvents {
     private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>();
+    private static final Logger LOGGER = LoggerFactory.getLogger(ModEvents.class);
 
     // Done with the help of https://github.com/CoFH/CoFHCore/blob/1.19.x/src/main/java/cofh/core/event/AreaEffectEvents.java
     // Don't be a jerk License
@@ -28,18 +31,23 @@ public class ModEvents {
         if(mainHandItem.getItem() instanceof HammerItem hammer && player instanceof ServerPlayer serverPlayer) {
             BlockPos originPos = event.getPos();
             if(HARVESTED_BLOCKS.contains(originPos)) {
+                LOGGER.info("Exiting hammer block break event");
                 return;
             }
 
-            for(BlockPos pos : HammerItem.getBlocksToBeDestroyed(0, originPos, serverPlayer)) {
-                if(pos == originPos || !hammer.isCorrectToolForDrops(mainHandItem, event.getLevel().getBlockState(pos))) {
+            LOGGER.info("Starting breaking blocks from {}", originPos);
+            for(BlockPos pos : HammerItem.getBlocksToBeDestroyed(1, originPos, serverPlayer)) {
+                if(pos.equals(originPos) || !hammer.isCorrectToolForDrops(mainHandItem, event.getLevel().getBlockState(pos))) {
+                    LOGGER.info("Skipping destroying block {}", pos);
                     continue;
                 }
 
+                LOGGER.info("Destroying block {}", pos);
                 HARVESTED_BLOCKS.add(pos);
                 serverPlayer.gameMode.destroyBlock(pos);
                 HARVESTED_BLOCKS.remove(pos);
             }
+            LOGGER.info("Ended breaking blocks from {}", originPos);
         }
     }
 
