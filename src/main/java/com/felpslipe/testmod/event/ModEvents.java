0628c2d4d0ab31de35143863903aae4d9415799d
FelpisLipe <176348835+FelpisLipe@users.noreply.github.com>
2025-03-07 20:19:49 -0300

    Removed loggers

--- src/main/java/com/felpslipe/testmod/event/ModEvents.java
+++ src/main/java/com/felpslipe/testmod/event/ModEvents.java
@@ -19,7 +19,6 @@ import java.util.Set;
 @EventBusSubscriber(modid = TestMod.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
 public class ModEvents {
     private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>();
-    private static final Logger LOGGER = LoggerFactory.getLogger(ModEvents.class);
 
     // Done with the help of https://github.com/CoFH/CoFHCore/blob/1.19.x/src/main/java/cofh/core/event/AreaEffectEvents.java
     // Don't be a jerk License
@@ -31,23 +30,17 @@ public class ModEvents {
         if(mainHandItem.getItem() instanceof HammerItem hammer && player instanceof ServerPlayer serverPlayer) {
             BlockPos originPos = event.getPos();
             if(HARVESTED_BLOCKS.contains(originPos)) {
-                LOGGER.info("Exiting hammer block break event");
                 return;
             }
 
-            LOGGER.info("Starting breaking blocks from {}", originPos);
             for(BlockPos pos : HammerItem.getBlocksToBeDestroyed(1, originPos, serverPlayer)) {
                 if(pos.equals(originPos) || !hammer.isCorrectToolForDrops(mainHandItem, event.getLevel().getBlockState(pos))) {
-                    LOGGER.info("Skipping destroying block {}", pos);
                     continue;
                 }
 
-                LOGGER.info("Destroying block {}", pos);
                 HARVESTED_BLOCKS.add(pos);
-                serverPlayer.gameMode.destroyBlock(pos);
                 HARVESTED_BLOCKS.remove(pos);
             }
-            LOGGER.info("Ended breaking blocks from {}", originPos);
         }
     }
 
